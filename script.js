ce888970d8074287a481c7334e3a9f7fd131e4a2
Beast Code <beastcode9999@gmail.com>
2022-11-24 23:04:45 +0900

    add JavaScript for footer element

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const mode = document.getElementById('mode');
 const share = document.querySelector('.header__share')
 // header container 2
 console.log(share)
+
+
+const footer = document.querySelector('.footer__year');
+console.log(footer)
+let year = new Date().getFullYear()
+footer.textContent = year;
