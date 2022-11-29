9d5bdadc4d949b88540182883bbf2575c3a1f794
Beast Code <beastcode9999@gmail.com>
2022-11-29 20:47:33 +0900

    add selectors for player x

--- script.js
+++ script.js
@@ -3,9 +3,13 @@
 const mode = document.getElementById('mode');
 const share = document.querySelector('.header__share')
 // header container 2
-console.log(share)
+const playerX = document.querySelector('.player player--x');
+const playerXScore = document.querySelector('.player__score--x');
 
 
+
+
+// footer script
 const footer = document.querySelector('.footer__year');
 console.log(footer)
 let year = new Date().getFullYear()
