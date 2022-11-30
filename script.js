32e031fdbd67ee3f3d09a27c2095ac40413328df
Beast Code <beastcode9999@gmail.com>
2022-11-30 21:00:06 +0900

    add headerChoiceText

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const playerX = document.querySelector('.player--x');
 const playerXScore = document.querySelector('.player__score--x');
 const playerO = document.querySelector('.player--o');
 const playerOScore = document.querySelector('.player__score--o');
-
+// header container 3
+const headerChoiceText = document.querySelector('.header__choice-text');
 
 
 // footer script
